5a4e7c6a5d3868884c03c34b4688b91e1c93c967
Abueesp <abueesp@gmail.com>
2017-05-24 00:06:51 +0200

    Create MarsennePrimes.py

---
+++ MarsennePrimes.py
@@ -0,0 +1,19 @@
+## Mersenne Primes Code ##
+## Series: Arithmetic Primes ##
+## Author: Abueesp ##
+## Date: Tue, May 23 ##
+## GNU License ##
+
+scriptname = 'Mersenne Prime Code'
+prompt = '> '
+
+print "Hello, I'm the %scriptname, a pretty dumb code for easy testing of Mersenne primes." % (scriptname)
+
+print "Introduce a prime number (remember, 1 and 0 are not prime numbers): "
+prime = int(raw_input(prompt))
+
+print "How many iterations do you want to calculate?:"
+iterations = int(raw_input(prompt))
+for i in range(iterations):
+	prime=(2**prime)-1
+	print prime
