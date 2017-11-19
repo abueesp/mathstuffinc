29a2c17335a6df556a95544c0228b99f195e773b
abueesp <abueesp@gmail.com>
2017-11-19 14:28:10 +0100

    Create WoodallPrimes.py

---
+++ WoodallPrimes.py
@@ -0,0 +1,19 @@
+## Mersenne Primes Code ##
+## Series: Arithmetic Primes ##
+## Author: Abueesp ##
+## Date: Tue, May 23 2015 ##
+## License: CC NC-BY-SA ##
+
+scriptname = 'Woodall Prime Code'
+prompt = '> '
+
+print "Hello, I'm the %scriptname, a pretty dumb code for easy testing of Woodall primes (n*2^n-1)." % (scriptname)
+
+print "Introduce a prime number (remember, 1 and 0 are not prime numbers): "
+prime = int(raw_input(prompt))
+
+print "How many iterations do you want to calculate?:"
+iterations = int(raw_input(prompt))
+for i in range(iterations):
+	prime=prime*(2**prime)-1
+	print prime
